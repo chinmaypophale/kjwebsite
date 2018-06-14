3fc085d654cd8a4c73294baf98b102d20d1834b1
chinmaypophale <37659306+chinmaypophale@users.noreply.github.com>
2018-06-14 16:45:59 +0530

    Php code and xampp connections done

---
+++ KJ/contactinfo.php
@@ -0,0 +1,25 @@
+<?php
+
+
+$server = "localhost";
+$user = "root";
+$pass = "";
+$db = "kaijing";
+$conn = mysqli_connect($server,$user,$pass,$db);
+
+		$name = mysqli_real_escape_string($conn,$_POST['name']);
+		$email = mysqli_real_escape_string($conn,$_POST['email']);
+		$phone = mysqli_real_escape_string($conn,$_POST['phone']);
+		$message = mysqli_real_escape_string($conn,$_POST['message']);
+	 
+ 
+ 
+	$sql = "INSERT INTO leadinfo (name,email,phone,message) VALUES ('$name', '$email','$phone','$message');";
+	mysqli_query($conn, $sql);
+
+
+	
+	header("Location: thanks.html");
+
+
+	?>
